cbf40f4ab422670fefef72b98d31dd566bf7a22d
mattia <mattia24.studio@gmail.com>
2020-07-06 18:37:39 +0200

    added

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var nome = prompt('Inserisci Nome');
+var cognome = prompt('Inserisci Cognome');
+var colore = prompt('Inserisci il tuo colore');
+document.getElementById('dati').innerHTML=nome + '' +cognome + '' + colore + '19';
